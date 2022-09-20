6aac0ed0a6bf08824f4b72baf7cb4fdf1ad929a5
namvt002 <viettrung0601@gmail.com>
2022-09-20 13:08:28 +0700

    Product detail

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer/footer.component";
 import Nav from "./components/Nav";
 import route from "./Router";
 
@@ -16,6 +17,7 @@ function App() {
 			<Routes>
 				{menu}
 			</Routes>
+			<Footer />
     </div>
   );
 }
--- src/pages/Home/Dashboard/ProductDetail/index.jsx
+++ src/pages/Home/Dashboard/ProductDetail/index.jsx
@@ -1,15 +1,115 @@
 import React from 'react'
 import './index.scss';
+
 export default function ProductDetail() {
   return (
-    <div style={{width: '100%'}}>
-      <div class="breadcrumb_background">
-        <div class="title_full">
-          <div class="container a-center">
-            <p class="title_page">Tp. Hồ Chí Minh</p>
+    <>
+     <div style={{width: '100%' }}>
+        <div className="breadcrumb_background">
+          <div className="title_full">
+            <div className="container a-center">
+              <p className="title_page">Tp. Hồ Chí Minh</p>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <section className="bread-crumb">
+            <span className="crumb-border" />
+            <div className="container-product-detail">
+              <div className="row">
+                <div className="col-xs-12 a-left">
+                  <ul className="breadcrumb">
+                    <li className="home">
+                      <a href="#"><span>Trang chủ</span></a>
+                      <span className="mr_lr">&nbsp;/&nbsp;</span>
+                    </li>
+                    <li>
+                      <a href="#"><span>Tp. Hồ Chí Minh</span></a>
+                      <span className="mr_lr">&nbsp;/&nbsp;</span>
+                    </li>
+                    <li><strong><span>Cho thuê căn hộ, biệt thự cao cấp</span></strong></li><li>
+                    </li></ul>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row gx-5">
+          <div className="col">
+            <div className="product-detail-left product-images col-xs-12 col-sm-12 col-md-6 col-lg-6">
+              <div className="col_large_default large-image">
+                <div className="iframe" />
+                <div id="panorama" className="hidden panorama">
+                  <div id="controls">
+                    <div className="ctrl" id="pan-up"><i className="fa fa-chevron-up" /></div>
+                    <div className="ctrl" id="pan-down"><i className="fa fa-chevron-down" /></div>
+                    <div className="ctrl" id="pan-left"><i className="fa fa-chevron-left" /></div>
+                    <div className="ctrl" id="pan-right"><i className="fa fa-chevron-right" /></div>
+                    <div className="ctrl" id="zoom-in"><i className="fa fa-plus" /></div>
+                    <div className="ctrl" id="zoom-out"><i className="fa fa-minus" /></div>
+                    <div className="ctrl" id="fullscreen"><i className="fa fa-expand" /></div>
+                  </div>
+                </div>
+                <a className="checkurl" href="../bizweb.dktcdn.net/thumb/1024x1024/100/336/794/products/6b92a6.jpg?v=1540398749457" data-rel="prettyPhoto[product-gallery]" src="//bizweb.dktcdn.net/thumb/1024x1024/100/336/794/products/6b.jpg?v=1540398749457">
+                  <img className="img-responsive" id="img_01" src="https://bizweb.dktcdn.net/100/336/794/products/6b.jpg?v=1540398749457" alt="Cho thuê biệt thự hiện đại" href="//bizweb.dktcdn.net/thumb/1024x1024/100/336/794/products/6b.jpg?v=1540398749457" />
+                </a>
+                <div className="hidden">
+                  <div className="item">
+                    <a href="../bizweb.dktcdn.net/100/336/794/products/0524yosemite-1300ra34c.jpg?v=1540398752110" data-image="https://bizweb.dktcdn.net/100/336/794/products/0524yosemite-1300r.jpg?v=1540398752110" data-rel="prettyPhoto[product-gallery]">
+                    </a>
+                  </div>
+                  <div className="item">
+                    <a href="../bizweb.dktcdn.net/100/336/794/products/1516262865-a61b9c37-c84e-49b7-ad53-cd2389f8da052542.jpg?v=1540398752867" data-image="https://bizweb.dktcdn.net/100/336/794/products/1516262865-a61b9c37-c84e-49b7-ad53-cd2389f8da05.jpg?v=1540398752867" data-rel="prettyPhoto[product-gallery]">
+                    </a>
+                  </div>
+                  <div className="item">
+                    <a href="../bizweb.dktcdn.net/100/336/794/products/jkh-ac4e3f5d-f0c9-49c4-9ab2-cebde2e63b472b7c.jpg?v=1540398753440" data-image="https://bizweb.dktcdn.net/100/336/794/products/jkh-ac4e3f5d-f0c9-49c4-9ab2-cebde2e63b47.jpg?v=1540398753440" data-rel="prettyPhoto[product-gallery]">
+                    </a>
+                  </div>
+                  <div className="item">
+                    <a href="../bizweb.dktcdn.net/100/336/794/products/800x5009e06.jpg?v=1540398754280" data-image="https://bizweb.dktcdn.net/100/336/794/products/800x500.jpg?v=1540398754280" data-rel="prettyPhoto[product-gallery]">
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="section owl_nav_custome2">
+                <div id="gallery_02" className="owl-carousel owl-theme thumbnail-product thumb_product_details not-thuongdq owl-loaded owl-drag">
+                  <div className="owl-stage-outer owl-height" style={{ height: 90 }}><div className="owl-stage" style={{ transform: 'translate3d(0px, 0px, 0px)', transition: 'all 0.25s ease 0s', width: 654 }}><div className="owl-item active" style={{ width: '120.75px', marginRight: 10 }}><div className="itemer">
+                    <div className="button_views_image block" href="#" data-link="https://bizweb.dktcdn.net/100/336/794/products/0524yosemite-1300r.jpg?v=1540398752110" title="Xem ảnh 360 độ">
+                      <img src="../bizweb.dktcdn.net/100/336/794/themes/692935/assets/36072dd.png?1638695199389" alt="Ảnh 306 độ" />
+                      <span>Ảnh 360°</span>
+                    </div>
+                  </div></div><div className="owl-item active" style={{ width: '120.75px', marginRight: 10 }}><div className="item">
+                    <a href="javascript:void(0)" data-image="https://bizweb.dktcdn.net/100/336/794/products/6b.jpg?v=1540398749457" data-zoom-image="../bizweb.dktcdn.net/thumb/1024x1024/100/336/794/products/6b92a6.jpg?v=1540398749457" className="zoomGalleryActive">
+                      <img data-img="//bizweb.dktcdn.net/thumb/1024x1024/100/336/794/products/6b.jpg?v=1540398749457" src="//bizweb.dktcdn.net/thumb/small/100/336/794/products/6b.jpg?v=1540398749457" data-lazyload="//bizweb.dktcdn.net/thumb/small/100/336/794/products/6b.jpg?v=1540398749457" alt="Cho thuê biệt thự hiện đại" />
+                    </a>
+                  </div></div><div className="owl-item active" style={{ width: '120.75px', marginRight: 10 }}><div className="item">
+                    <a href="javascript:void(0)" data-image="https://bizweb.dktcdn.net/100/336/794/products/1516262865-a61b9c37-c84e-49b7-ad53-cd2389f8da05.jpg?v=1540398752867" data-zoom-image="../bizweb.dktcdn.net/thumb/1024x1024/100/336/794/products/1516262865-a61b9c37-c84e-49b7-ad53-cd2389f8da052542.jpg?v=1540398752867" className>
+                      <img data-img="//bizweb.dktcdn.net/thumb/1024x1024/100/336/794/products/1516262865-a61b9c37-c84e-49b7-ad53-cd2389f8da05.jpg?v=1540398752867" src="//bizweb.dktcdn.net/thumb/small/100/336/794/products/1516262865-a61b9c37-c84e-49b7-ad53-cd2389f8da05.jpg?v=1540398752867" data-lazyload="//bizweb.dktcdn.net/thumb/small/100/336/794/products/1516262865-a61b9c37-c84e-49b7-ad53-cd2389f8da05.jpg?v=1540398752867" alt="Cho thuê biệt thự hiện đại" />
+                    </a>
+                  </div></div><div className="owl-item active" style={{ width: '120.75px', marginRight: 10 }}><div className="item">
+                    <a href="javascript:void(0)" data-image="https://bizweb.dktcdn.net/100/336/794/products/jkh-ac4e3f5d-f0c9-49c4-9ab2-cebde2e63b47.jpg?v=1540398753440" data-zoom-image="../bizweb.dktcdn.net/thumb/1024x1024/100/336/794/products/jkh-ac4e3f5d-f0c9-49c4-9ab2-cebde2e63b472b7c.jpg?v=1540398753440" className>
+                      <img data-img="//bizweb.dktcdn.net/thumb/1024x1024/100/336/794/products/jkh-ac4e3f5d-f0c9-49c4-9ab2-cebde2e63b47.jpg?v=1540398753440" src="//bizweb.dktcdn.net/thumb/small/100/336/794/products/jkh-ac4e3f5d-f0c9-49c4-9ab2-cebde2e63b47.jpg?v=1540398753440" data-lazyload="//bizweb.dktcdn.net/thumb/small/100/336/794/products/jkh-ac4e3f5d-f0c9-49c4-9ab2-cebde2e63b47.jpg?v=1540398753440" alt="Cho thuê biệt thự hiện đại" />
+                    </a>
+                  </div></div><div className="owl-item" style={{ width: '120.75px', marginRight: 10 }}><div className="item">
+                    <a href="javascript:void(0)" data-image="https://bizweb.dktcdn.net/100/336/794/products/800x500.jpg?v=1540398754280" data-zoom-image="../bizweb.dktcdn.net/thumb/1024x1024/100/336/794/products/800x5009e06.jpg?v=1540398754280">
+                      <img data-img="//bizweb.dktcdn.net/thumb/1024x1024/100/336/794/products/800x500.jpg?v=1540398754280" src="//bizweb.dktcdn.net/thumb/small/100/336/794/products/800x500.jpg?v=1540398754280" data-lazyload="//bizweb.dktcdn.net/thumb/small/100/336/794/products/800x500.jpg?v=1540398754280" alt="Cho thuê biệt thự hiện đại" />
+                    </a>
+                  </div></div></div></div><div className="owl-nav"><div className="owl-prev disabled">prev</div><div className="owl-next">next</div></div><div className="owl-dots disabled" /></div>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="p-3 border bg-light">Custom column padding</div>
           </div>
         </div>
       </div>
-    </div>
+
+     
+    </>
+
   )
 }
